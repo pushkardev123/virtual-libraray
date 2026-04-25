21635744a2aa0aebd3de18d44d6430e765140555
Pushkar Dev <76200195+pushkardev123@users.noreply.github.com>
2026-04-25 20:00:17 +0530

    /build

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,15 @@
+"use strict";(()=>{var e={};e.id=660,e.ids=[660],e.modules={7471:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var s=n(997),r=n(6859);function i(){return(0,s.jsxs)(r.Html,{lang:"en",children:[(0,s.jsxs)(r.Head,{children:[s.jsx("script",{dangerouslySetInnerHTML:{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-5QWP7ZKZ');`}}),s.jsx("script",{dangerouslySetInnerHTML:{__html:`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '1788102748497505');
+                    fbq('track', 'PageView');`}})]}),(0,s.jsxs)("body",{children:[s.jsx("noscript",{children:s.jsx("iframe",{src:"https://www.googletagmanager.com/ns.html?id=GTM-5QWP7ZKZ",height:"0",width:"0",style:{display:"none",visibility:"hidden"}})}),s.jsx("noscript",{children:s.jsx("img",{height:"1",width:"1",style:{display:"none"},src:"https://www.facebook.com/tr?id=1788102748497505&ev=PageView&noscript=1"})}),s.jsx(r.Main,{}),s.jsx(r.NextScript,{})]})]})}},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},997:e=>{e.exports=require("react/jsx-runtime")},5315:e=>{e.exports=require("path")}};var t=require("../webpack-runtime.js");t.C(e);var n=e=>t(t.s=e),s=t.X(0,[6859],()=>n(7471));module.exports=s})();
