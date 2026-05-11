4dd591a2eced89084f565ca5f88a04df62d2dc34
Pushkar Dev <76200195+pushkardev123@users.noreply.github.com>
2026-05-11 13:22:45 +0530

    .next delete

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/v2/neet-pg":["static\u002Fchunks\u002Fpages\u002Fv2\u002Fneet-pg.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fv2\u002Fneet-pg"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
